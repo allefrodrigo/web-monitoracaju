bc0a05d7acfe7775f111e80d6e86719b241957fe
allefschmidt <dev.allefschmidt@gmail.com>
2023-10-06 10:28:38 -0300

    :rocket: lançamento em produção

--- src/examples/Cards/InfoCards/PhotoCard/index.js
+++ src/examples/Cards/InfoCards/PhotoCard/index.js
@@ -2,12 +2,14 @@ import PropTypes from "prop-types";
 import Card from "@mui/material/Card";
 import MDBox from "components/MDBox";
 import "./PhotoCard.css"; // Importe o arquivo de estilos
+const backendUrl = process.env.REACT_APP_BACKEND_URL;
+
 
 function PhotoCard({ src, alt, tipo, ...imgProps }) {
-//  console.log(`192.168.0.9:3000/images/${src}`)
+
   return (
     <div className="photo-card"  >
-      {tipo === 'envio' ? (<img  src={`http://192.168.1.8:3000/images/${src}`} alt={alt} {...imgProps} />):(
+      {tipo === 'envio' ? (<img  src={`${backendUrl}/photos/images/${src}`} alt={alt} {...imgProps} />):(
         <img src="https://www.placebrinquedos.com.br/lib/img/imagem-nao-disponivel.jpg" alt={alt} {...imgProps} />)}
     </div>
   );
